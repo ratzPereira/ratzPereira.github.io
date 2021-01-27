39a74a55109cff31bc59b36051e922ab3f7627ea
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-01-27 22:50:04 -0100

    almost done

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+// show and hide menu
+
+const navMenu = document.getElementById('nav-menu')
+const toggleMenu = document.getElementById('nav-toggle')
+const closeMenu = document.getElementById('nav-close')
+
+//show
+toggleMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('show')
+})
+
+//hidden
+closeMenu.addEventListener('click', () => {
+  navMenu.classList.remove('show')
+})
+
+//remove menu
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  navMenu.classList.remove('show')
+}
+
+navLink.forEach((e) => e.addEventListener('click', linkAction))
+
+//scroll sections active link
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+
+    sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  })
+}
